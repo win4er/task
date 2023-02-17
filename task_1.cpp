d5a73a9d0aeca717ff4203474f13934135e656d4
win4er <104188509+win4er@users.noreply.github.com>
2023-02-17 14:54:38 +0300

    Delete task_1.cpp

--- task_1.cpp
+++
@@ -1,8 +0,0 @@
-#include "task_1.hpp"
-int main() {
-    int ar[1000];
-    fillRandom(ar, 100);
-    BubbleSort2(ar, 100);
-    showEl(ar, 100);
-    cout << ar[countMax(ar, 100)] << ": " << countDig(ar, 100, countMax(ar, 100)) << " | " << ar[countMin(ar, 100)] << ": " << countDig(ar, 100, countMin(ar, 100)) << endl;
-}

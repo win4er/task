47bb416da2d879813c981e43842586a7cabd4c85
win4er <104188509+win4er@users.noreply.github.com>
2023-02-17 14:59:53 +0300

    Create testsorts.h

---
+++ testsorts.h
@@ -0,0 +1,16 @@
+#ifndef _testsorts_h_
+#define _testsorts_h_
+
+#include <iostream>
+#include "sorts.h"
+#include "statarray.h"
+#include "arraywork.h"
+#include "task_2.h"
+
+void test_BubbleSort();
+void test_InsertionSort();
+void test_SelectionSort();
+void test_QuickSort();
+void test_MergeSort();
+
+#endif

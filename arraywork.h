3887d9c66d6571a26f0b0485bd99043fd6c93c40
win4er <104188509+win4er@users.noreply.github.com>
2023-02-17 14:56:50 +0300

    Create arraywork.h

---
+++ arraywork.h
@@ -0,0 +1,12 @@
+#ifndef _arraywork_h_
+#define _arraywork_h_
+
+#include <iostream>
+#include <time.h>
+
+void swap(int&, int&);
+void random_Fill_ar(int*, int, int = 0, int = 255);
+void show_ar(int*, int, char = ' ');
+
+
+#endif

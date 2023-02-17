f081d01d050b3393b6970a4d93ae4e22e5c3cda7
win4er <104188509+win4er@users.noreply.github.com>
2023-02-17 14:57:10 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "task_2.h"
+#include "arraywork.h"
+#include "sorts.h"
+#include "testsorts.h"
+#include "statarray.h"
+
+int main(){
+	test_BubbleSort();
+	test_InsertionSort();
+	test_SelectionSort();
+	test_QuickSort();
+	test_MergeSort();
+	return 0;
+}
